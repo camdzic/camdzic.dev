c0d7ca53bcef71bd5b19761e3067b0b0ef60492e
camdzic <acamdzic0@proton.me>
2024-01-14 22:12:52 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 
 import { GeistSans } from 'geist/font/sans';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata: Metadata = {
   title: "Hey, I'm camdzic",
   description: 'A software engineer from Bosnia & Herzegovina.'
@@ -25,7 +27,10 @@ export default function RootLayout({
         className={`${geist.className} min-h-screen flex items-center justify-center`}
       >
         <main className="max-w-5xl mx-auto">
-          <section className="px-4 sm:px-6 lg:px-8 py-16">{children}</section>
+          <section className="px-4 sm:px-6 lg:px-8 py-16">
+            {children}
+            <Analytics />
+          </section>
         </main>
       </body>
     </html>
